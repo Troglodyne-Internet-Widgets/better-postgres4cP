6a2495c6b4aa919f5fd8253cdc677a7b9780ea4f
Andy Baugh <andy@troglodyne.net>
2020-02-19 20:24:31 -0600

    Dab
    Got me some js now

--- install/install.pl
+++ install/install.pl
@@ -11,7 +11,7 @@ use Cpanel::Mkdir ();
 
 my $repo_dir = Cwd::abs_path(File::Basename::dirname(__FILE__) . "/../");
 Cpanel::Mkdir::ensure_directory_existence_and_mode("/var/cpanel/apps", 0755);
-my %install_to = qw{lib /var/cpanel/perl templates/config /var/cpanel/templates/troglodyne templates/ui /usr/local/cpanel/whostmgr/docroot/templates/troglodyne cgi /usr/local/cpanel/whostmgr/docroot/cgi/troglodyne img /usr/local/cpanel/whostmgr/docroot/addon_plugins};
+my %install_to = qw{lib /var/cpanel/perl templates/config /var/cpanel/templates/troglodyne templates/ui /usr/local/cpanel/whostmgr/docroot/templates/troglodyne cgi /usr/local/cpanel/whostmgr/docroot/cgi/troglodyne img /usr/local/cpanel/whostmgr/docroot/addon_plugins js /usr/local/cpanel/whostmgr/docroot/cgi/troglodyne/js};
 foreach my $dir ( keys(%install_to) ) {
     Cpanel::Mkdir::ensure_directory_existence_and_mode( $install_to{$dir}, 0755 );
     my @cmd = ( qw{rsync -r}, "$repo_dir/$dir/", $install_to{$dir} );
