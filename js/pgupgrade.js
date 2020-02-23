285f59a80fac24e2c61860130bc427e5bd896976
Andy Baugh <andy@troglodyne.net>
2020-02-23 10:49:43 -0600

    Build the table

--- js/pgupgrade.js
+++ js/pgupgrade.js
@@ -2,6 +2,8 @@ function versionHandler () {
     'use strict';
     let obj = JSON.parse(this.responseText);
     if(obj.result === 1) {
+
+        // Construct version warning/display
         let pgVersion = obj.data.installed_version.major + '.' + obj.data.installed_version.minor;
         let elem = document.getElementById('psqlVersion');
         let html = pgVersion;
@@ -10,6 +12,31 @@ function versionHandler () {
             html += " -- You are using a version of PostgreSQL Server that is no longer supported! Immediate upgrade reccomended.";
         }
         elem.innerHTML = html;
+
+        // Now let's build the table
+        let rows = '';
+        for ( var version of Object.keys(obj.data.available_versions) ) {
+            rows +=
+`<tr id="pgVersionRow-${version}">
+    <td>
+        <input type="radio" name="selectedVersion" value="${version}"></input>
+        ${version}
+    </td>
+    <td>
+        Lorem Ipsum
+    </td>
+    <td>
+        Community
+    </td>
+    <td>
+        ${obj.data.available_versions[version].release}
+    </td>
+    <td>
+        ${obj.data.available_versions[version].EOL}
+    </td>
+</tr>`;
+        }
+        document.querySelector('#upgradeForm > table > tbody').innerHTML = rows;
     } else {
         console.log(obj.error);
     }
