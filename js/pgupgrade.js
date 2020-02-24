48ea1cc556d03d0963a606dce9a65fac273ffbab
Andy Baugh <andy@troglodyne.net>
2020-02-23 18:14:15 -0600

    Table now looking real nice

--- js/pgupgrade.js
+++ js/pgupgrade.js
@@ -6,7 +6,7 @@ function versionHandler () {
         // Construct version warning/display
         let pgVersion = obj.data.installed_version.major + '.' + obj.data.installed_version.minor;
         let elem = document.getElementById('psqlVersion');
-        let html = pgVersion;
+        let html = `<strong>${pgVersion}</strong>`;
         if( parseFloat(pgVersion) < parseFloat(obj.data.minimum_supported_version) ) {
             elem.classList.add('callout', 'callout-danger');
             html += " -- You are using a version of PostgreSQL Server that is no longer supported! Immediate upgrade reccomended.";
@@ -15,24 +15,23 @@ function versionHandler () {
 
         // Now let's build the table
         let rows = '';
-        for ( var version of Object.keys(obj.data.available_versions) ) {
+        for ( var version of Object.keys(obj.data.available_versions).sort(function(a,b) { return parseFloat(b) - parseFloat(a) }) ) {
             rows +=
 `<tr id="pgVersionRow-${version}">
     <td>
-        <input type="radio" name="selectedVersion" value="${version}"></input>
+        <input type="radio" name="selectedVersion" value="${version}" onclick="document.getElementById('submit').disabled = false;"></input>
         ${version}
     </td>
+    <td><ul>`;
+        obj.data.available_versions[version].features.forEach(function(feature) {
+            rows += `<li>${feature}</li>`;
+        });
+    rows += `</ul></td>
     <td>
-        Lorem Ipsum
+        ${new Date(obj.data.available_versions[version].release * 1000).toLocaleString(undefined, { year: 'numeric', month: 'long', day: 'numeric' })}
     </td>
     <td>
-        Community
-    </td>
-    <td>
-        ${obj.data.available_versions[version].release}
-    </td>
-    <td>
-        ${obj.data.available_versions[version].EOL}
+        ${new Date(obj.data.available_versions[version].EOL * 1000).toLocaleString(undefined, { year: 'numeric', month: 'long', day: 'numeric' })}
     </td>
 </tr>`;
         }
@@ -42,6 +41,7 @@ function versionHandler () {
     }
 }
 
+document.getElementById('submit').disabled = true;
 var oReq = new XMLHttpRequest();
 oReq.addEventListener("load", versionHandler);
 oReq.open("GET", "api.cgi?module=Postgres&function=get_postgresql_versions");
--- lib/Troglodyne/CpPostgreSQL.pm
+++ lib/Troglodyne/CpPostgreSQL.pm
@@ -16,11 +16,31 @@ our $MINIMUM_SUPPORTED_VERSION = '9.5';
 
 # Times are in seconds since epoch, as that allows easier localization.
 our %SUPPORTED_VERSIONS_MAP = (
-    '9.5' => { 'release' => 1452146400, 'EOL' => 1613023200 },
-    '9.6' => { 'release' => 1475125200, 'EOL' => 1636610400 },
-    '10'  => { 'release' => 1507179600, 'EOL' => 1668060000 },
-    '11'  => { 'release' => 1539838800, 'EOL' => 1699509600 },
-    '12'  => { 'release' => 1570078800, 'EOL' => 1731564000 },
+    '9.5' => {
+        'release'  => 1452146400,
+        'EOL'      => 1613023200,
+        'features' => [ 'INSERTS with constraint conflicts can be turned into UPDATEs or ignored', 'Row-level security control', 'Adds Block Range Indexes' ],
+    },
+    '9.6' => {
+        'release'  => 1475125200,
+        'EOL'      => 1636610400,
+        'features' => [ 'Synchronous replication now allows multiple standby servers', 'Full-text search can now search for phrases', 'Substantial performance improvements' ],
+     },
+    '10'  => {
+        'release'  => 1507179600,
+        'EOL'      => 1668060000,
+        'features' => [ 'Logical replication using publish/subscribe', 'Significant general performance improvements', 'Stronger password authentication', 'Improved monitoring and control' ],
+     },
+    '11'  => {
+        'release'  => 1539838800,
+        'EOL'      => 1699509600,
+        'features' => [ 'Improvements to partitioning and parallelism', 'SQL stored procedures that support embedded transactions', 'Many other useful performance improvements' ]
+     },
+    '12'  => {
+        'release'  => 1570078800,
+        'EOL'      => 1731564000,
+        'features' => [ 'General performance improvements', 'Stored generated columns', 'Support for the SQL/JSON path language', 'New authentication features' ]
+    },
 );
 
 # The BS that cPanel will be installing with /scripts/installpostgres
